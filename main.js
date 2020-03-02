38a0b6f1aedb94f6ad22d147e06da266eb36e03a
rabbihosain <56104056+rabbihosain@users.noreply.github.com>
2020-03-02 12:31:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Sticky Navigatioin 
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+});
+
+
+// counter up
+
+let nCount = function (selector) {
+    $(selector).each(function () {
+        $(this).animate({
+            Conter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: "swing",
+            step: function (value) {
+                $(this).text(Math.ceil(value));
+            }
+        })
+    });
+}
+
+let a = 0;
+$(window).scroll(function () {
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect>h1");
+    }
+})
